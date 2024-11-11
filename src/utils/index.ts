5b4d884518041397a49f0e99d91dd6a2f69d0e82
LookNow <paramas.wae.th@gmail.com>
2024-11-11 16:40:36 +0700

    🚀 first: first code

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { cn } from "./tailwind";
+export { getFormattedDate } from "./date";
+export { generateToc } from "./generateToc";
+export type { TocItem } from "./generateToc";
+export { elementHasClass, toggleClass, rootInDarkMode } from "./domElement";
